94d9575ad77d1ed09b728668a4c61d2552f0f919
Alex Knott <alextknott@gmail.com>
2025-01-31 21:28:24 +0000

    Configure aws security groups

---
+++ terraform/security.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "rds" {
+  name        = "automated-agency-rds"
+  description = "Security group for RDS PostgreSQL instance"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow PostgreSQL traffic from EC2 security group
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2.id]
+  }
+
+  tags = {
+    Name = "automated-agency-rds-sg"
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "automated-agency-ec2"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "automated-agency-ec2-sg"
+  }
+}
